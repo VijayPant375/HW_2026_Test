d2f085220b47ce95936b9feb5f6cd8d4f16f79f4
Vijay Pant <vijaypant486@gmail.com>
2026-08-22 21:00:33 +0530

    Add JSON-driven player movement

---
+++ Assets/Scripts/DoofusController.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DoofusController : MonoBehaviour
+{
+    [SerializeField] private TextAsset configFile;
+
+    private float speed;
+
+    private void Start()
+    {
+        GameConfig config = JsonUtility.FromJson<GameConfig>(configFile.text);
+        speed = config.player_data.speed;
+    }
+
+    private void Update()
+    {
+        Vector2 input = Vector2.zero;
+
+        if (Keyboard.current != null)
+        {
+            if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed)
+                input.x -= 1;
+
+            if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed)
+                input.x += 1;
+
+            if (Keyboard.current.sKey.isPressed || Keyboard.current.downArrowKey.isPressed)
+                input.y -= 1;
+
+            if (Keyboard.current.wKey.isPressed || Keyboard.current.upArrowKey.isPressed)
+                input.y += 1;
+        }
+
+        Vector3 movement = new Vector3(input.x, 0f, input.y).normalized;
+
+        transform.position += movement * speed * Time.deltaTime;
+    }
+
+    [System.Serializable]
+    private class GameConfig
+    {
+        public PlayerData player_data;
+    }
+
+    [System.Serializable]
+    private class PlayerData
+    {
+        public float speed;
+    }
+}
