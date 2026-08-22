7ebd7523ef543995d238818eefe4543ac45118d6
Vijay Pant <vijaypant486@gmail.com>
2026-08-22 21:26:23 +0530

    Complete Level 1 pulpit spawning

---
+++ Assets/Scripts/PulpitSpawner.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using UnityEngine;
+
+public class PulpitSpawner : MonoBehaviour
+{
+    [System.Serializable]
+    public class PulpitData
+    {
+        public float min_pulpit_destroy_time;
+        public float max_pulpit_destroy_time;
+        public float pulpit_spawn_time;
+    }
+
+    [System.Serializable]
+    public class DiaryData
+    {
+        public PulpitData pulpit_data;
+    }
+
+    public GameObject pulpitPrefab;
+    public TextAsset configFile;
+
+    private float spawnTime;
+    private Transform lastPulpit;
+
+    private void Start()
+    {
+        LoadConfig();
+
+        GameObject existingPulpit = GameObject.FindGameObjectWithTag("Pulpit");
+
+        if (existingPulpit != null)
+        {
+            lastPulpit = existingPulpit.transform;
+        }
+
+        StartCoroutine(SpawnPulpits());
+    }
+
+    private void LoadConfig()
+    {
+        DiaryData diary = JsonUtility.FromJson<DiaryData>(configFile.text);
+
+        spawnTime = diary.pulpit_data.pulpit_spawn_time;
+    }
+
+    private IEnumerator SpawnPulpits()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnTime);
+
+            if (GameObject.FindGameObjectsWithTag("Pulpit").Length < 2)
+            {
+                SpawnNextPulpit();
+            }
+        }
+    }
+
+    private void SpawnNextPulpit()
+    {
+        Vector3 spawnPosition;
+
+        if (lastPulpit == null)
+        {
+            spawnPosition = Vector3.zero;
+        }
+        else
+        {
+            Vector3[] directions =
+            {
+                Vector3.forward,
+                Vector3.back,
+                Vector3.left,
+                Vector3.right
+            };
+
+            Vector3 direction = directions[Random.Range(0, directions.Length)];
+
+            spawnPosition = lastPulpit.position + direction * 9f;
+        }
+
+        GameObject newPulpit = Instantiate(
+            pulpitPrefab,
+            spawnPosition,
+            Quaternion.identity
+        );
+
+        newPulpit.tag = "Pulpit";
+
+        lastPulpit = newPulpit.transform;
+    }
+}
